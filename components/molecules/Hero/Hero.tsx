34bf164a56db776c7f49f728f72bcbdf6a270819
Lenny <lnmuzzupappa@gmail.com>
2023-11-02 01:46:21 -0300

    feat(Hero): add heading animations and implement bubbles

--- components/molecules/Hero/Hero.tsx
+++ components/molecules/Hero/Hero.tsx
@@ -1,7 +1,56 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import { Headline } from "@atoms/Headline/Headline";
+import { Bubble } from "@atoms/Bubble/Bubble";
 import styles from "./Hero.module.css";
 
 export const Hero = () => {
+  const headlineRef = useRef<HTMLHeadingElement>(null);
+  const bubbleLeftRef = useRef<HTMLSpanElement>(null);
+  const bubbleRightRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const headline = headlineRef.current;
+    if (!headline) return;
+
+    const headlineSpans = headline.querySelectorAll("span");
+    const supplySpan = headlineSpans[1];
+
+    const tl = gsap.timeline({
+      defaults: {
+        duration: 1,
+        ease: "power4.out",
+      },
+    });
+
+    handleHeadlineAnimation({ tl, headlineSpans });
+
+    if (!bubbleLeftRef.current || !bubbleRightRef.current) return;
+  }, []);
+
+  const handleHeadlineAnimation = ({
+    tl,
+    headlineSpans,
+  }: {
+    tl: gsap.core.Timeline;
+    headlineSpans: NodeListOf<HTMLSpanElement>;
+  }) => {
+    tl.fromTo(
+      headlineSpans,
+      {
+        opacity: 1,
+        y: 100,
+      },
+      {
+        opacity: 1,
+        stagger: 0.1,
+        y: 0,
+      }
+    );
+  };
+
   return (
     <section className={styles.hero}>
       <Headline
@@ -11,10 +60,18 @@ export const Hero = () => {
           { text: "Basement", variant: "fill" },
           { text: "Supply", variant: "outline", position: "center" },
         ]}
+        selfRef={headlineRef}
       />
-
-      {/* <small className="bubble">EST</small>
-      <small className="bubble">2K19</small> */}
+      <Bubble
+        selfRef={bubbleLeftRef}
+        className={styles.heroBubbleLeft}
+        copy="EST"
+      />
+      <Bubble
+        selfRef={bubbleRightRef}
+        className={styles.heroBubbleRight}
+        copy="2K19"
+      />
     </section>
   );
 };
