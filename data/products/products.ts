198eb7e84cd32ae9b89b2a12f2b3854d77aa609f
Leandro Muzzupappa <lnmuzzupappa@gmail.com>
2023-11-02 18:54:34 -0300

    feat(products): create mock data

---
+++ data/products/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: "d26k7Q",
+    name: "Black t-shirt",
+    price: 7.95,
+    image: "/products/remera.png",
+  },
+  {
+    id: "4j3n4Q",
+    name: "Balck hoodie",
+    price: 13,
+    image: "/products/buzo.png",
+  },
+  {
+    id: "5n3k7Q",
+    name: "Black cap",
+    price: 23,
+    image: "/products/gorrita.png",
+  },
+];
