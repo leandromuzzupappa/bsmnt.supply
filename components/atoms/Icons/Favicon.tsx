d6659228428432619cc3e0efedf6b26769d63eab
Lenny <lnmuzzupappa@gmail.com>
2023-11-01 23:05:36 -0300

    refactor(icons): update attribute spelling errors

--- components/atoms/Icons/Favicon.tsx
+++ components/atoms/Icons/Favicon.tsx
@@ -4,78 +4,18 @@ export default function FavIcon({ className, selfRef }: IconsProps) {
   return (
     <span ref={selfRef} className={className}>
       <svg
-        width="128"
-        height="127"
-        viewBox="0 0 128 127"
+        width="260"
+        height="260"
+        viewBox="0 0 260 260"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g id="Group 7437">
-          <g id="Group">
-            <path
-              id="Vector"
-              d="M63.9941 125.661C98.5999 125.661 126.653 97.755 126.653 63.3307C126.653 28.9064 98.5999 0.999985 63.9941 0.999985C29.3884 0.999985 1.33484 28.9064 1.33484 63.3307C1.33484 97.755 29.3884 125.661 63.9941 125.661Z"
-              stroke="white"
-              stroke-width="1.51786"
-              strokeMiterlimit="10"
-            />
-            <path
-              id="Vector_2"
-              d="M63.9939 125.661C88.5188 125.661 108.4 97.755 108.4 63.3307C108.4 28.9064 88.5188 0.999985 63.9939 0.999985C39.469 0.999985 19.5876 28.9064 19.5876 63.3307C19.5876 97.755 39.469 125.661 63.9939 125.661Z"
-              stroke="white"
-              stroke-width="1.51786"
-              strokeMiterlimit="10"
-            />
-            <path
-              id="Vector_3"
-              d="M63.9938 125.661C78.4536 125.661 90.1756 97.755 90.1756 63.3307C90.1756 28.9064 78.4536 0.999985 63.9938 0.999985C49.5341 0.999985 37.8121 28.9064 37.8121 63.3307C37.8121 97.755 49.5341 125.661 63.9938 125.661Z"
-              stroke="white"
-              stroke-width="1.51786"
-              strokeMiterlimit="10"
-            />
-            <path
-              id="Vector_4"
-              d="M63.9942 125.661C68.3731 125.661 71.9229 97.755 71.9229 63.3307C71.9229 28.9064 68.3731 0.999985 63.9942 0.999985C59.6153 0.999985 56.0656 28.9064 56.0656 63.3307C56.0656 97.755 59.6153 125.661 63.9942 125.661Z"
-              stroke="white"
-              stroke-width="1.51786"
-              strokeMiterlimit="10"
-            />
-          </g>
-          <g id="Group_2">
-            <path
-              id="Vector_5"
-              d="M28.9141 11.7807H99.2168"
-              stroke="white"
-              stroke-width="1.51786"
-              strokeMiterlimit="10"
-              stroke-linecap="round"
-            />
-            <path
-              id="Vector_6"
-              d="M9.5199 32.406H118.439"
-              stroke="white"
-              stroke-width="1.51786"
-              strokeMiterlimit="10"
-              stroke-linecap="round"
-            />
-            <path
-              id="Vector_7"
-              d="M9.57629 94.2265H118.382"
-              stroke="white"
-              stroke-width="1.51786"
-              strokeMiterlimit="10"
-              stroke-linecap="round"
-            />
-            <path
-              id="Vector_8"
-              d="M28.7428 114.852H99.2167"
-              stroke="white"
-              stroke-width="1.51786"
-              strokeMiterlimit="10"
-              stroke-linecap="round"
-            />
-          </g>
-        </g>
+        <rect width="260" height="260" fill="black" />
+        <path
+          d="M75.7181 141.791C76.8101 129.525 84.6732 123.619 99.5257 123.619C119.62 123.619 123.333 128.162 123.333 148.379V165.642C123.333 185.632 119.62 190.175 99.5257 190.175C84.8917 190.175 77.0286 185.632 75.7181 176.773V141.791ZM34 43.2054V224.93H74.4075V192.22C78.7759 215.844 92.9732 227.656 117.218 227.656C157.844 227.656 167.017 217.434 167.017 166.551V147.016C167.017 96.1327 158.062 85.9107 119.402 85.9107C93.1916 85.9107 80.5233 92.0439 75.4996 113.169V43.2054H34Z"
+          fill="#FAFAFA"
+        />
+        <path d="M232 186.314H194.869V224.93H232V186.314Z" fill="#FAFAFA" />
       </svg>
     </span>
   );
