68ec41eb1be69761188cc72c81e6d1c9163da2d6
Leandro Muzzupappa <lnmuzzupappa@gmail.com>
2023-11-03 13:52:37 -0300

    feat(layout): add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,34 @@ const grotesque = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Pepitos",
+  metadataBase: new URL("https://bsmnt-supply.vercel.app/"),
+  title: "Basement Supply",
   description: "Wholesome pepitos page",
+  keywords: ["next.js", "typescript"],
+  authors: [
+    {
+      name: "Leandro Muzzupappa",
+      url: "https://lmuzzupappa.vercel.app/",
+    },
+  ],
+  creator: "facuperezm",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "/OG.png",
+    title: "Basement Supply",
+    description: "Wholesome pepitos page",
+    siteName: "Basement Supply",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Basement Supply",
+    description: "Wholesome pepitos page",
+    images: ["https://bsmnt-supply.vercel.app/OG.png"],
+  },
+  icons: {
+    icon: "https://bsmnt-supply.vercel.app/b-favicon.ico",
+  },
 };
 
 export default function RootLayout({
