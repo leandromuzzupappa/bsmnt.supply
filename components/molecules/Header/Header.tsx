c8f0f3080e5f1cb17483b7e799dc6eb5f35f9b2f
Leandro Muzzupappa <lnmuzzupappa@gmail.com>
2023-11-03 05:35:20 -0300

    feat(Header): add logic to handle the cart open and close

--- components/molecules/Header/Header.tsx
+++ components/molecules/Header/Header.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import Link from "next/link";
 import { Icon } from "@atoms/Icons/Icon";
 import { CartButton } from "@atoms/CartButton/CartButton";
-
+import { CartModal } from "@molecules/CartModal/CartModal";
 import styles from "./Header.module.css";
 import { IconNames } from "@/data/enums/Icons";
 
 export const Header = () => {
-  const eyeRef = useRef<HTMLSpanElement>(null);
+  const [cartOpen, setCartOpen] = useState(false);
+
+  const onCardButtonClick = () => {
+    setCartOpen(!cartOpen);
+  };
 
   return (
     <header className={styles.header}>
@@ -28,12 +32,14 @@ export const Header = () => {
       <div className={styles.headerIcons} aria-hidden="true">
         <Icon name={IconNames.RADIO} />
         <Icon name={IconNames.HD} />
-        <Icon name={IconNames.EYE} selfRef={eyeRef} />
+        <Icon name={IconNames.EYE} />
         <Icon name={IconNames.UHD} />
         <Icon name={IconNames.ALERT} />
       </div>
 
-      <CartButton />
+      <CartButton onClick={onCardButtonClick} isOpen={cartOpen} />
+
+      <CartModal isOpen={cartOpen} onCloseCart={() => setCartOpen(false)} />
     </header>
   );
 };
