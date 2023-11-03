d3554883846b07563d7491bce25ee3f83972b44d
Leandro Muzzupappa <lnmuzzupappa@gmail.com>
2023-11-03 05:34:08 -0300

    feat(CartButton): add animation when open the cart modal

--- components/atoms/CartButton/CartButton.tsx
+++ components/atoms/CartButton/CartButton.tsx
@@ -1,14 +1,27 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styles from "./CartButton.module.css";
 
-export const CartButton = () => {
+interface ICartButtonProps {
+  isOpen: boolean;
+  onClick: () => void;
+}
+
+export const CartButton = ({ isOpen, onClick }: ICartButtonProps) => {
+  const closeBtnRef = useRef<HTMLButtonElement>(null);
+  const cartBtnRef = useRef<HTMLButtonElement>(null);
+
   const [cartCount, setCartCount] = useState(12);
 
   return (
-    <button className={styles.cartButton}>
-      Cart (<span>{cartCount}</span>)
+    <button className={styles.cartButton} onClick={onClick} data-open={isOpen}>
+      <span className={styles.cartButtonTextClose} aria-hidden={!isOpen}>
+        → Close
+      </span>
+      <span className={styles.cartButtonText} aria-hidden={isOpen}>
+        Cart (<span>{cartCount}</span>)
+      </span>
     </button>
   );
 };
